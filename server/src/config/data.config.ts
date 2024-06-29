28f227c64e7473c10ae4ff6b3ff9a85fc95bc52a
FerLopez <ferlopeztrdev@gmail.com>
2024-06-28 20:53:31 -0400

    update backend

--- server/src/config/data.config.ts
+++ server/src/config/data.config.ts
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const {
-    PORT = 3000,
+    PORT,
     CORS_ORIGIN = 'http://localhost:5173',
-    DB_USER = 'root',
-    DB_HOST = 'localhost',
-    DB_PASSWORD = '',
-    DB_DATABASE = 'boardify'
+    DB_USER,
+    DB_HOST,
+    DB_PASSWORD,
+    DB_DATABASE
 } = process.env;
